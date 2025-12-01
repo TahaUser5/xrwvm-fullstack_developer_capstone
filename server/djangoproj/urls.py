82c80105c047eb003bb57da4c01f159a2d73d5e0
Taha Tanvir <165398422+TahaUser5@users.noreply.github.com>
2025-12-01 17:26:46 +0000

    fixed python lint errors

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,24 +23,48 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Static pages
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    
+    path(
+        'about/',
+        TemplateView.as_view(template_name="About.html"),
+    ),
+    path(
+        'contact/',
+        TemplateView.as_view(template_name="Contact.html"),
+    ),
+
     # 1. ROOT PATH MUST BE HOME.HTML
-    path('', TemplateView.as_view(template_name="Home.html")),  
+    path(
+        '',
+        TemplateView.as_view(template_name="Home.html"),
+    ),
 
     # Backend API routes
     path('djangoapp/', include('djangoapp.urls')),
-    
+
     # 2. ONLY THESE PATHS SHOULD BE INDEX.HTML
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    
+    path(
+        'register/',
+        TemplateView.as_view(template_name="index.html"),
+    ),
+    path(
+        'login/',
+        TemplateView.as_view(template_name="index.html"),
+    ),
+
     # 3. dealers dynamic pages
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path(
+        'dealers/',
+        TemplateView.as_view(template_name="index.html"),
+    ),
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html"),
+    ),
 
     # Add this line for the post review page
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html"),
+    ),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
