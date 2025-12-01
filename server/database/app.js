1209ef1eb97d3242aeed575289a5c31e3b47bc27
Taha Tanvir <165398422+TahaUser5@users.noreply.github.com>
2025-12-01 21:24:04 +0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -117,3 +118,4 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
