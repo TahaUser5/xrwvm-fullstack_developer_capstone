e9cc602645326b650d646eb9ac51a65fa879a6e5
Taha Tanvir <165398422+TahaUser5@users.noreply.github.com>
2025-12-01 21:53:30 +0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,34 +1,24 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    # Many-to-One relationship to CarMake
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    
-    # Dealer Id refers to a dealer created in Cloudant database
-    # (Important: This field is required to link specific cars to specific dealers)
-    dealer_id = models.IntegerField() 
-    
     name = models.CharField(max_length=100)
-    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    # Year with validation (2015 - 2023)
     year = models.IntegerField(
         default=2023,
         validators=[
@@ -38,4 +28,4 @@ class CarModel(models.Model):
     )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name}"
+        return self.name
